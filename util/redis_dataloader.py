f24ce2d75eccb99b42934e34e85fbbac73399ad3
gacerioni <gacerioni@gmail.com>
2024-05-13 11:20:15 -0300

    Initial commit

---
+++ util/redis_dataloader.py
@@ -0,0 +1,12 @@
+import json
+from time import sleep
+
+import requests
+
+with open('data/people.json', encoding='utf-8') as f:
+    people = json.loads(f.read())
+
+for person in people:
+    r = requests.post('http://127.0.0.1:5000/person/new', json=person)
+    sleep(1)
+    print(f"Created person {person['first_name']} {person['last_name']} with ID {r.text}")
